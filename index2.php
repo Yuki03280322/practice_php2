c31bd7dd31132df0222e03070ae5dbfcffc4a455
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2021-02-24 23:32:44 +0900

    7日目

--- index2.php
+++ index2.php
@@ -17,7 +17,6 @@
 
 <main>
 <h2>Practice</h2>
-<pre>
 <?php
 try {
   $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8',
@@ -25,3 +24,26 @@ try {
 } catch(PDOException $e) {
   echo 'DB接続エラー: ' . $e->getMessage();
 }
+
+$memos = $db->query('SELECT * FROM memos ORDER BY id DESC ');
+// memosテーブルからid情報を取得し降順に並び替え変数に格納
+?>
+<article>
+  <?php
+  while ($memo = $memos->fetch()): ?>
+    <p><a href="memo.php?id=<?php print($memo['id']); ?>"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
+    <time><?php print($memo['created_at']); ?></time>
+    <hr>
+  <?php endwhile; ?>
+</article>
+</main>
+<!--
+articleタグ・・様々記事を表示するためのタグ
+$memos->fetch();・・memosレコードセットに入っているデータを１件ずつ取り出しmemoへ格納
+timeタグで「時」を表すコンテンツを囲むことで検索エンジンが「時」を表しているコンテンツであることを認識できるようになる
+mb_substr・・文字数を制限、3つのパラメーター→元となる文字列＋開始位置＋文字数0,50で最初から50文字まで出力
+URLにつく?はウェブの仕様であり（HTTPプロトコルの仕様）、URLの後ろに？をつけてそのページに渡したいパラメーターを続けると、
+ウェブページに内容を渡すことができるという仕組み
+ -->
+</body>
+</html>
---
+++ memo.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../php/css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+try {
+  $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8',
+  'root', 'root');
+} catch(PDOException $e) {
+  echo 'DB接続エラー: ' . $e->getMessage();
+}
+
+$id = $_REQUEST['id'];
+if (!is_numeric($id) || $id<=0) {
+  print('1以上の数字で指定してください');
+  exit();
+}
+// URLパラメーターを使用する場合、想定した値以外の値を指定されないように排除しDBに投げないようにする
+
+$memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+$memos->execute(array($id));
+$memo = $memos->fetch();
+?>
+<article>
+<pre><?php print($memo['memo']); ?></pre>
+<a href="index2.php">戻る</a>
+</article>
+</main>
+<!--
+URLパラメーターを取得するためには$_REQUEST,$_GETどちらかになる。今後どこかのタイミングでPOSTでIDを渡す可能性を加味し$_REQUESTを使用
+その時prepareメソッドを使用し、id=?、executeメソッドを使用し?を置き換えるパラメーターを指定し安全にURLパラメーターを渡す
+prepareメソッドで使用する?はPHPの仕様であり、プレースホルダーという仕組み
+プレースホルダーとはSQL文の中の変動する箇所に使用し、その部分はあくまで値として処理され万が一不正な値が入力されてもSQL命令に関わるような特殊文字は無効化（エスケープ）される
+ -->
+</body>    
+</html>
